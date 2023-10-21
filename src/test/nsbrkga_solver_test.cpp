a31d2b03c8c7d56deef3dc1d1b75d69d8452998d
Luis H. P. Mendes <luishpmendes@gmail.com>
2023-10-21 18:32:15 -0300

    Changing default path relinking parameters
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- src/test/nsbrkga_solver_test.cpp
+++ src/test/nsbrkga_solver_test.cpp
@@ -56,12 +56,12 @@ int main() {
         assert(solver.num_populations == 3);
         assert(solver.exchange_interval == 200);
         assert(solver.num_exchange_individuals == 3);
-        assert(solver.pr_type == NSBRKGA::PathRelinking::Type::PERMUTATION);
+        assert(solver.pr_type == NSBRKGA::PathRelinking::Type::BINARY_SEARCH);
         assert(typeid(*solver.pr_dist_func) ==
-                typeid(NSBRKGA::KendallTauDistance));
+                typeid(NSBRKGA::EuclideanDistance));
         assert(fabs(solver.pr_percentage - 0.20) <
                 std::numeric_limits<double>::epsilon());
-        assert(solver.pr_interval == 0);
+        assert(solver.pr_interval == 500);
         assert(solver.shake_interval == 200);
         assert(fabs(solver.shake_intensity - 0.33) < 
             std::numeric_limits<double>::epsilon());
