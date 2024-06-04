e37c5929203c53e20d8c49835e51f1594103fec3
Luis H. P. Mendes <luishpmendes@gmail.com>
2024-06-04 19:41:16 -0300

    Update instance_test.cpp with additional test cases for larger instances

--- src/test/instance_test.cpp
+++ src/test/instance_test.cpp
@@ -18,7 +18,10 @@ int main() {
                                        "instances/zlt_500_4.txt",
                                        "instances/zlt_750_2.txt",
                                        "instances/zlt_750_3.txt",
-                                       "instances/zlt_750_4.txt"}) {
+                                       "instances/zlt_750_4.txt",
+                                       "instances/zlt_1000_2.txt",
+                                       "instances/zlt_1000_3.txt",
+                                       "instances/zlt_1000_4.txt"}) {
         std::cout << filename << std::endl;
 
         ifs.open(filename);
@@ -32,7 +35,7 @@ int main() {
         assert(instance.num_dimensions >= 2);
         assert(instance.num_dimensions <= 4);
         assert(instance.num_items >= 100);
-        assert(instance.num_items <= 750);
+        assert(instance.num_items <= 1000);
         assert(instance.is_valid());
     }
 
